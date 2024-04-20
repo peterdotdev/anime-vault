e08d97cce13124b4f8a47e22ee3e73bbacdb75e2
peterdotdev <peterdotdev@gmail.com>
2024-04-19 22:05:29 -0300

    anime vault

---
+++ app/action.tsx
@@ -0,0 +1,17 @@
+"use server";
+
+import { AnimeProp, AnimeCard2 } from "@/components/AnimeCard";
+
+const MAX_LIMIT = 8;
+
+export async function fetchAnime(page: number) {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&order=popularity`
+  );
+
+  const data = await response.json();
+
+  return data.map((anime: AnimeProp, index: number) => (
+    <AnimeCard2 key={anime.id} anime={anime} index={index} />
+  ));
+}
